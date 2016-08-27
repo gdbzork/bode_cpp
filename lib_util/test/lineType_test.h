a5ce8a7022e45c9f746c2e8c2fce670a1be779c6
Gord Brown <gdbzork@gmail.com>
2016-08-27 19:16:52 +0100

    add lineType tests

--- lib_util/test/lineType_test.h
+++ lib_util/test/lineType_test.h
@@ -6,8 +6,29 @@
 class LineTypeTest: public CxxTest::TestSuite {
   public:
 
-    void testSanity(void) {
+    void testUnix(void) {
+      std::string fn = "testdata/sheet_unix.txt";
+      bode::TextType lt = bode::TextTypeChecker::sget(fn);
+      TS_ASSERT_EQUALS(bode::UNIX,lt);
     }
 
+    void testMac(void) {
+      std::string fn = "testdata/sheet_mac.txt";
+      bode::TextType lt = bode::TextTypeChecker::sget(fn);
+      TS_ASSERT_EQUALS(bode::MAC,lt);
+    }
+
+    void testDos(void) {
+      std::string fn = "testdata/sheet_dos.txt";
+      bode::TextType lt = bode::TextTypeChecker::sget(fn);
+      TS_ASSERT_EQUALS(bode::DOS,lt);
+    }
+
+    void testMixed(void) {
+      std::string fn = "testdata/sheet_mixed.txt";
+      bode::TextType lt = bode::TextTypeChecker::sget(fn);
+      TS_ASSERT_EQUALS(bode::MIXED,lt);
+    }
 
 };
+
